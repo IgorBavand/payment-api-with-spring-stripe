c53ca2b4fc9e0e0a6f9d3ec3c218b4c10dbc1ff2
Guerreiro <igor.silva@xbrain.com.br>
2023-08-21 08:04:16 -0300

    config: config cors

---
+++ src/main/java/com/payment/payment_example/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.payment.payment_example.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(-1);
+    }
+
+}
