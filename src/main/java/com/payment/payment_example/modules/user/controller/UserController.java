7160c9f0100d8178c10b87ccbac407b138437e32
Guerreiro <igor.silva@xbrain.com.br>
2023-08-22 04:19:09 -0300

    docs: add responses in product controller

--- src/main/java/com/payment/payment_example/modules/user/controller/UserController.java
+++ src/main/java/com/payment/payment_example/modules/user/controller/UserController.java
@@ -3,9 +3,7 @@ package com.payment.payment_example.modules.user.controller;
 import com.payment.payment_example.modules.product.model.Product;
 import com.payment.payment_example.modules.user.model.User;
 import com.payment.payment_example.modules.user.service.UserService;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +22,10 @@ public class UserController {
     @ApiOperation(httpMethod = "POST", value = "register new user", response = User[].class)
     @ApiImplicitParams({
             @ApiImplicitParam(name = "Authorization", value = "Bearer {token}", required = true, paramType = "header")
+    })
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "User registred", response = Product.class),
+            @ApiResponse(code = 400, message = "Username already used.")
     })
     public ResponseEntity<?> registerUser(@RequestBody User newUser) {
         return ResponseEntity.ok().body(service.registerUser(newUser));
